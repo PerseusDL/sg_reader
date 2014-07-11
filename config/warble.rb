0c39fa913c035fe24fa22f46d0897b0d36699a46
LFDM <1986gh@gmail.com>
2014-07-11 13:56:28 +0200

    Add warble config

---
+++ config/warble.rb
@@ -0,0 +1,15 @@
+# Disable Rake-environment-task framework detection by uncommenting/setting to false
+# Warbler.framework_detection = false
+
+# Warbler web application assembly configuration file
+Warbler::Config.new do |config|
+  # This is finally working with the advent of warbler version 1.4.1
+  config.bundler = true
+
+  # Set JRuby to run in 2.0 mode.
+  config.webxml.jruby.compat.version = "2.0"
+
+  # also used to name the war - defaults to the name and a version string
+  # right now, which is not that useful
+  config.jar_name = 'sg'
+end
